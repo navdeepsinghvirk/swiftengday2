e6b157c49593fa5023cecae8f9c4635925ee9000
navdeepsinghvirk <35242807+navdeepsinghvirk@users.noreply.github.com>
2018-01-30 20:45:30 -0500

    Add files via upload

--- Contents.swift
+++ Contents.swift
@@ -2,85 +2,35 @@
 
 import UIKit
 
-var str = "Hello, playground"
-var a = [1,2,3,4,5]
-print (a[0])
-a.append(6)
-print(a[5])
 
 
-var b = [10,20,30]
+var letters = Set<Character>()
+letters.insert("A")
+letters.insert("b")
+print("Count \(letters.count)")
 
-a = a + b
-for i in a
-{
-    print(i)
-}
-
-var c : [Int]=[]
-
-print(a.count)
+letters.insert("A")
+print("Count \(letters.count)")
 
 
-//c.append(8)
+var str = "Hello World😊"
 
-print(c.count)
+var a = "\u{24}"
+var b = "\u{1F496}"
+var c = "\u{65}"
+print(a,b,c)
 
-var d:[Int]!
-print(a.count)
 
-//d?.append(8)
-print(d?.count ?? 0)
+let longstring = """
+abfjsfsk bfsb
+sdfsfsbfsdbkfdjs
+sjfksfbsfbsd
+""";
 
-print("--- NEW ---")
-for i in a[5...]
-{
-    print(i)
-}
-print("--- NEW ---")
-for i in a[3..<6]
-{
-    print(i)
-}
-var f = [2...5]
-print("----Special NEW---")
-for i in f
-{
-    print(i)
-}
-var e = a[2...5]
-print("--- NEW ---")
-for i in e
-{
-    print(i)
-}
-print("--- ONE ---")
-print(a[2])
-print("--- TWO ---")
-print(e[2])
+print(longstring)
 
-e[2] = 900
+print("\"HELLO\",NAVI")
 
-print("--- THREE ---")
-print(a[2])
-print("--- FOUR ---")
-print(e[2])
+var s = "Welcome to Canada"
 
-print("Size of e \(e.count)")
 
-var threeDoubles = Array(repeating: 0.0,count: 3)
-
-for (k,v) in a.enumerated()
-{
-    print("Index : \(k)-->\(v)")
-}
-if let ov = a.updateValue("London",forKey: "city")
-{
-    print("The old value for city was \(ov).")
-}
-print("Extract Keys and store in array")
-let keys = [String](a.keys)
-
-for i in keys{
-    print(i)
-}
